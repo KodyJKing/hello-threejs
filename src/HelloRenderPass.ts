72696056b2e244117535fb0390d99f276d7f40c0
kjk <kody.j.king@gmail.com>
2021-08-04 23:56:44 -0700

    added silhouette effect

--- src/HelloRenderPass.ts
+++ src/HelloRenderPass.ts
@@ -2,22 +2,35 @@ import * as THREE from "three"
 import { WebGLRenderer, WebGLRenderTarget } from "three"
 import { Pass, FullScreenQuad } from "three/examples/jsm/postprocessing/Pass"
 
+function pixelRenderTarget() {
+}
+
 export default class HellRenderPass extends Pass {
 
     fsQuad: FullScreenQuad
-    resolution: { x: number, y: number }
+    resolution: THREE.Vector2
     scene: THREE.Scene
     camera: THREE.Camera
     rgbRenderTarget: WebGLRenderTarget
+    // normalRenderTarget: WebGLRenderTarget
 
-    constructor( resolution: { x: number, y: number }, scene: THREE.Scene, camera: THREE.Camera ) {
+    constructor( resolution: THREE.Vector2, scene: THREE.Scene, camera: THREE.Camera ) {
         super()
         this.resolution = resolution
         this.fsQuad = new FullScreenQuad( this.material() )
         this.scene = scene
         this.camera = camera
 
-        const rgbRenderTarget = this.rgbRenderTarget = new WebGLRenderTarget( resolution.x, resolution.y )
+        const rgbRenderTarget = this.rgbRenderTarget = new WebGLRenderTarget(
+            resolution.x, resolution.y,
+            {
+                depthTexture: new THREE.DepthTexture(
+                    resolution.x,
+                    resolution.y
+                ),
+                depthBuffer: true
+            }
+        )
         rgbRenderTarget.texture.format = THREE.RGBAFormat
         rgbRenderTarget.texture.minFilter = THREE.NearestFilter
         rgbRenderTarget.texture.magFilter = THREE.NearestFilter
@@ -33,9 +46,10 @@ export default class HellRenderPass extends Pass {
         renderer.setRenderTarget( this.rgbRenderTarget )
         renderer.render( this.scene, this.camera )
 
-        // The declarations for Three.js don't include Material.uniforms
         // @ts-ignore
         this.fsQuad.material.uniforms.tDiffuse.value = this.rgbRenderTarget.texture
+        // @ts-ignore
+        this.fsQuad.material.uniforms.tDepth.value = this.rgbRenderTarget.depthTexture
         if ( this.renderToScreen ) {
             renderer.setRenderTarget( null )
         } else {
@@ -48,21 +62,56 @@ export default class HellRenderPass extends Pass {
     material() {
         return new THREE.ShaderMaterial( {
             uniforms: {
-                tDiffuse: { value: null }
+                tDiffuse: { value: null },
+                tDepth: { value: null },
+                resolution: {
+                    value: new THREE.Vector4(
+                        this.resolution.x,
+                        this.resolution.y,
+                        1 / this.resolution.x,
+                        1 / this.resolution.y,
+                    )
+                }
             },
             vertexShader:
-                `varying vec2 vUv;
+                `
+                varying vec2 vUv;
                 void main() {
                     vUv = uv;
                     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-                }`,
+                }
+                `,
             fragmentShader:
-                `uniform sampler2D tDiffuse;
+                `
+                uniform sampler2D tDiffuse;
+                uniform sampler2D tDepth;
+                uniform vec4 resolution;
                 varying vec2 vUv;
+
+                float getDepth(int x, int y) {
+                    return texture2D( tDepth, vUv + vec2(x, y) * resolution.zw ).r;
+                }
+
+                float saturate(float x) {
+                    return clamp(x, 0.0, 1.0);
+                }
+
+                float depthEdgeIndicator() {
+                    float depth = getDepth(0, 0);
+                    float diff = 0.0;
+                    diff += abs(depth - getDepth(1, 0));
+                    diff += abs(depth - getDepth(-1, 0));
+                    diff += abs(depth - getDepth(0, 1));
+                    diff += abs(depth - getDepth(0, -1));
+                    return saturate(diff);
+                }
+
                 void main() {
                     vec4 texel = texture2D( tDiffuse, vUv );
-                    gl_FragColor = texel;
-                }`
+                    float edgeIndicator = depthEdgeIndicator();
+                    gl_FragColor = texel * (1.0 + edgeIndicator);
+                }
+                `
         } )
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three"
+import { Vector2 } from "three"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
@@ -14,16 +15,22 @@ init()
 
 function init() {
 
-    let outResolution = { x: window.innerWidth, y: window.innerHeight }
-    let aspectRatio = outResolution.x / outResolution.y
+    let renderResolution = new Vector2( 256, 256 )
+    let screenResolution = new Vector2( window.innerWidth, window.innerHeight )
+    let aspectRatio = screenResolution.x / screenResolution.y
 
     // camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 )
     camera = new THREE.OrthographicCamera( -aspectRatio, aspectRatio, 1, -1, .01, 10 )
-    camera.position.z = 1
+    camera.position.z = 0.9480823308542135
+    camera.position.y = 0.7907471388920719
+    let angle = Math.atan2( camera.position.y, camera.position.z )
+    console.log( angle )
+    console.log( Math.PI / angle )
 
     scene = new THREE.Scene()
 
-    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 )
+    const sideLength = .4
+    geometry = new THREE.BoxGeometry( sideLength, sideLength, sideLength )
     material = new THREE.MeshNormalMaterial()
 
     mesh = new THREE.Mesh( geometry, material )
@@ -38,7 +45,7 @@ function init() {
     composer.addPass( new RenderPass( scene, camera ) )
     // composer.addPass( new GlitchPass() )
     // composer.addPass( new HelloWorldPass() )
-    composer.addPass( new HelloRenderPass( { x: 256, y: 256 }, scene, camera ) )
+    composer.addPass( new HelloRenderPass( renderResolution, scene, camera ) )
 
 
     controls = new OrbitControls( camera, renderer.domElement )
@@ -48,7 +55,8 @@ animate()
 function animate() {
     requestAnimationFrame( animate )
     mesh.rotation.y = Math.PI / 4
-    mesh.rotation.x = .61547
+    // mesh.rotation.x = Math.PI / 6
+    // mesh.rotation.x = .61547
     // renderer.render( scene, camera )
     composer.render()
 }
